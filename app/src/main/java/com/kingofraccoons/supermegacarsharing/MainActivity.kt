170b73356b4726e8bee3879ddaa5b6b5e225a22f
KingOfRaccoon <zvezdakov2015@yandex.ru>
2023-06-25 00:24:49 +0300

    Init commit and add settings xml

---
+++ app/src/main/java/com/kingofraccoons/supermegacarsharing/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.kingofraccoons.supermegacarsharing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kingofraccoons.supermegacarsharing.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/kingofraccoons/supermegacarsharing/ui/main/settings/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.kingofraccoons.supermegacarsharing.ui.main.settings
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment: Fragment() {
+}
